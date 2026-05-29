67e05f7a4ea80dab8f1f0ec2474d8e7646cd5213
unknown <Unoesc@JBALA-LAB043026.unoesc.lan>
2026-05-29 20:48:55 -0300

    Login 2

--- sistema/autenticacao/Main.java
+++ sistema/autenticacao/Main.java
@@ -27,19 +27,19 @@ public class Main {
 
             try {
                 Usuario usuario = authService.login(email, senha);
-                System.out.println("\n\u2705 Login efetuado com sucesso!");
+                System.out.println("Login efetuado com sucesso!");
                 System.out.println("Bem-vindo(a), " + usuario.getNome() + "!");
                 System.out.println("Perfil: " + usuario.getTipo());
                 logado = true; // Encerra o loop após o login com sucesso
             } catch (ValidacaoException e) {
-                System.out.println("\n\u274C Erro de valida\u00E7\u00E3o (KAN-12)!");
+                System.out.println("Erro de valida\u00E7\u00E3o (KAN-12)!");
                 System.out.println("Mensagem: " + e.getMessage());
                 System.out.println("Campo com problema: " + e.getCampoComProblema() + "\n");
             } catch (IllegalArgumentException e) {
-                System.out.println("\n\u274C Falha na autentica\u00E7\u00E3o!");
+                System.out.println("Falha na autentica\u00E7\u00E3o!");
                 System.out.println("Mensagem: " + e.getMessage() + "\n");
             } catch (Exception e) {
-                System.out.println("\n\u274C Erro inesperado: " + e.getMessage() + "\n");
+                System.out.println("Erro inesperado: " + e.getMessage() + "\n");
             }
         }
 
